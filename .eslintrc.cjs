a47ab1a0e08375026eca006ffe50ecc019942ec7
bitaKitsui <takaki_kitsui@bita.jp>
2022-09-30 10:12:07 +0900

    feat: setup lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
