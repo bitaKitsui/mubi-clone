22b4c94f32f1ad4b1ec9d352738f9ecd9b6b6639
bitaKitsui <takaki_kitsui@bita.jp>
2022-10-03 09:01:07 +0900

    feat: Button コンポーネント作成

--- src/components/atoms/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { Button } from "./Button";
-
-test("button 要素が正しく描画されていること", () => {
-  render(<Button />);
-  expect(screen.getByRole("button")).toBeInTheDocument();
-});
